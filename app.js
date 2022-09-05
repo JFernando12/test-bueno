bb0cc2da3a47c32b69d7ec8489ad5f3926b81952
fernando <fernandocastrejonh@gmail.com>
2022-09-05 06:13:57 -0500

    changes

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 // Needs correcting:
 // It is very important to have correctly formatted code to improve readability.
 // It is recommended to use a linter to identify errors: https://www.npmjs.com/package/eslint
-// if you are not using a module, the ideal is to uninstall it to optimize the code.
+// if you are not using a dependecy, the ideal is to uninstall it to optimize the code: https://docs.npmjs.com/uninstalling-packages-and-dependencies
 // if you don't use a script in the package.json the ideal is to remove it to prevent confusions.
-// An old version of the sqlite3 module is installed and it doesn't work.: https://docs.npmjs.com/updating-packages-downloaded-from-the-registry
+// An old version of the sqlite3 module is installed and it doesn't work: https://docs.npmjs.com/updating-packages-downloaded-from-the-registry
 
 // Excellent: Using capital letters for naming constants is a good practice!
 const HTTP_PORT = 3000;
